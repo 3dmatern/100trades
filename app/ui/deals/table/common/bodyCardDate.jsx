a7f96bae42a9d8cf90265df8d1d90b8a3471de1b
Denis Matern <denismatern@gmail.com>
2024-01-07 20:10:51 +0300

    FIX BodyCardDate, initHeaders

--- app/ui/deals/table/common/bodyCardDate.jsx
+++ app/ui/deals/table/common/bodyCardDate.jsx
@@ -1,12 +1,70 @@
-export default function BodyCardDate({ dealDate }) {
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+export default function BodyCardDate({ dealDate, name }) {
+    const cellRef = useRef(null);
+    const [active, setActive] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [date, setDate] = useState("");
+
+    const handleChange = ({ target }) => {
+        console.log(target.value);
+        setDate(target.value);
+    };
+
+    useEffect(() => {
+        if (dealDate) {
+            setDate(new Date(dealDate).toISOString().slice(0, 16));
+        }
+    }, [dealDate]);
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (cellRef.current && !cellRef.current.contains(e.target)) {
+                setOpen(false);
+                setActive(false);
+            }
+        };
+        const handleScroll = () => {
+            setOpen(false);
+            setActive(false);
+        };
+
+        document.addEventListener("click", handleClickOutside);
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="flex items-center justify-center border-r w-32 min-w-4 h-8 px-2 text-xs">
-            {dealDate &&
-                `${new Date(dealDate).getMonth() + 1}/${new Date(
-                    dealDate
-                ).getDate()}/${new Date(dealDate).getFullYear()}  ${new Date(
-                    dealDate
-                ).getHours()}:${new Date(dealDate).getMinutes()}`}
+        <div
+            ref={cellRef}
+            onClick={() => {
+                setActive(true);
+                setOpen(true);
+            }}
+            className={`flex items-center justify-center ${
+                active ? "border border-blue-800" : "border-r"
+            } w-36 min-w-4 h-8 px-2 text-xs`}
+        >
+            {open ? (
+                <input
+                    type="datetime-local"
+                    name={name}
+                    defaultValue={date}
+                    onChange={handleChange}
+                    onBlur={() => {
+                        setActive(false);
+                        setOpen(false);
+                    }}
+                    className="outline-none"
+                />
+            ) : (
+                date && date.split("T")[0] + " " + date.split("T")[1]
+            )}
         </div>
     );
 }
