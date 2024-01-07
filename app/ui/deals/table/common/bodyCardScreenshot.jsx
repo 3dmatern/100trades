a4cd0ee086c56bfc7437b6764dd12e563868e02b
Denis Matern <denismatern@gmail.com>
2024-01-07 15:31:42 +0300

    FIX BodyCardScreenshot

--- app/ui/deals/table/common/bodyCardScreenshot.jsx
+++ app/ui/deals/table/common/bodyCardScreenshot.jsx
@@ -1,4 +1,7 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function BodyCardScreenshot({
     dealImageSrc,
@@ -6,15 +9,67 @@ export default function BodyCardScreenshot({
     width,
     height,
 }) {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        const handleKyeDown = (e) => {
+            if (e.key === "Escape" && open) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener("keydown", handleKyeDown);
+        return () => {
+            document.removeEventListener("keydown", handleKyeDown);
+        };
+    }, [open]);
+
     return (
         <div className="flex items-center justify-center border-r w-24 min-w-4 h-8 px-2 text-xs">
-            {dealImageSrc && (
+            {dealImageSrc && !open && (
                 <Image
                     src={`/${dealImageSrc}`}
                     alt={imageAlt}
                     width={width}
                     height={height}
+                    onClick={handleClick}
+                    className="hover:scale-125 cursor-pointer"
                 />
+            )}
+            {dealImageSrc && (
+                <div
+                    className={`fixed w-screen h-screen z-50 top-0 left-0 bg-slate-900 p-4 ${
+                        !open
+                            ? "opacity-0 transform scale-0"
+                            : "opacity-100 transform scale-100"
+                    } transition-transform duration-300`}
+                >
+                    <button
+                        onClick={handleClick}
+                        className="absolute top-4 right-4 z-50 p-0.5 cursor-pointer text-cyan-500"
+                    >
+                        <Image
+                            src="./close.svg"
+                            alt="close"
+                            width={16}
+                            height={16}
+                        />
+                    </button>
+
+                    <p className="text-center text-sm text-white">
+                        {dealImageSrc}
+                    </p>
+
+                    <img
+                        src={`/${dealImageSrc}`}
+                        alt={imageAlt}
+                        className="mt-6 m-auto w-5/6"
+                    />
+                </div>
             )}
         </div>
     );
