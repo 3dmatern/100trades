ec4cf8254f152bcbc5502fb849ee2f14939497c3
Denis Matern <denismatern@gmail.com>
2024-01-10 22:17:18 +0300

    FIX auth.config

--- auth.config.js
+++ auth.config.js
@@ -6,14 +6,16 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnHomePage = nextUrl.pathname === "/";
+            const isOnRegistrationPage = nextUrl.pathname === "/registration";
+            const isOnDealsPage = nextUrl.pathname === "/deals";
+            const isOnProfilePage = nextUrl.pathname === "/profile";
 
-            if (isLoggedIn && isOnHomePage) {
+            if (isLoggedIn && (isOnHomePage || isOnRegistrationPage)) {
                 return Response.redirect(new URL("/deals", nextUrl));
-            } else if (isLoggedIn) {
-                return true;
-            } else {
-                return false; // Перенаправляем неаутентифицированных пользователей на страницу входа
+            } else if (!isLoggedIn && (isOnDealsPage || isOnProfilePage)) {
+                return Response.redirect(new URL("/", nextUrl));
             }
+            return true;
         },
     },
     providers: [], // Пока добавляем провайдеров с пустым массивом
