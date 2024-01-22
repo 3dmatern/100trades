e7186dac19ea77ff9a5c59b86ea514daa3218f79
Denis Matern <denismatern@gmail.com>
2024-01-22 23:05:16 +0300

    fix next.config

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "http",
-                hostname: "31.129.102.70",
-                port: "",
-                pathname: "/images/**",
-            },
-            {
-                protocol: "http",
-                hostname: "31.129.102.70",
-                port: "3001",
-                pathname: "/images/**",
-            },
-            {
-                protocol: "http",
                 hostname: "localhost",
                 port: "3001",
                 pathname: "/images/**",
