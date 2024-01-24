7d5c8d8a1bd5cc98817fa7c620223a13554a6ed2
Denis Matern <denismatern@gmail.com>
2024-01-25 02:56:02 +0300

    fix: authMiddleware to server

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,11 @@
 module.exports = (req, res, next) => {
     // Проверка на доверенный IP-адрес
-    const trustedIPs = ["::ffff:127.0.0.1", "100trades.ru", "::1"]; // добавлен '::1' для IPv6 localhost
+    const trustedIPs = [
+        "::ffff:127.0.0.1",
+        "100trades.ru",
+        "5.35.92.151",
+        "::1",
+    ]; // добавлен '::1' для IPv6 localhost
     const clientIP = req.ip || req.socket.remoteAddress;
 
     if (!trustedIPs.includes(clientIP)) {
