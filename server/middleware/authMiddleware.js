771f34d97ec3783eb2542cb8c7721eeaeed0165d
Denis Matern <denismatern@gmail.com>
2024-01-25 02:00:48 +0300

    fix: domen

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     // Проверка на доверенный IP-адрес
-    const trustedIPs = ["::ffff:127.0.0.1", "31.129.102.70", "::1"]; // добавлен '::1' для IPv6 localhost
+    const trustedIPs = ["::ffff:127.0.0.1", "100trades.ru", "::1"]; // добавлен '::1' для IPv6 localhost
     const clientIP = req.ip || req.socket.remoteAddress;
 
     if (!trustedIPs.includes(clientIP)) {
