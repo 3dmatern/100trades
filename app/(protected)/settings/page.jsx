f2b5cc54167e04849a302c195ef886b51705f699
Denis Matern <denismatern@gmail.com>
2024-02-07 13:22:55 +0300

    fix: autocomplete

--- app/(protected)/settings/page.jsx
+++ app/(protected)/settings/page.jsx
@@ -86,6 +86,7 @@ export default function SettingsPage() {
                                                     {...field}
                                                     disabled={isPending}
                                                     placeholder="bestTrader"
+                                                    autocomplete="off"
                                                 />
                                             </FormControl>
                                             <FormMessage />
