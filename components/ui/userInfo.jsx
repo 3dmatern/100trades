65e1dd270061d7a9d174e6e6807bd021a1290a35
Denis Matern <denismatern@gmail.com>
2024-02-07 10:25:49 +0300

    fix: email

--- components/ui/userInfo.jsx
+++ components/ui/userInfo.jsx
@@ -26,12 +26,6 @@ export function UserInfo({ user, label }) {
                     </p>
                 </div>
                 <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
-                    <p className="text-sm font-medium">Email</p>
-                    <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
-                        {user.email}
-                    </p>
-                </div>
-                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
                     <p className="text-sm font-medium">Роль</p>
                     <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
                         {user.role}
